9f16e5f0735634a419c3d8e962df3dd6bf5ddcfc
Chad Rossouw <chadrossouw7247@gmail.com>
2023-11-11 21:07:07 -0500

    fix(backend/events): changed createEvent to take date as parameter

--- app/src/main/java/com/example/demoapplication/handlers/subhandlers/admin/AdminEventsHandler.java
+++ app/src/main/java/com/example/demoapplication/handlers/subhandlers/admin/AdminEventsHandler.java
@@ -6,21 +6,24 @@ import com.example.demoapplication.helpers.Helper;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.Date;
+
 public class AdminEventsHandler extends EventsHandler {
 
     public AdminEventsHandler(FirebaseDatabase db, DatabaseReference ref) {
         super(db, ref);
     }
 
-    Event createEvent(String title, String description) {
+    Event createEvent(String title, String description, Date date) {
         // Generate a unique DB reference for new object.
         DatabaseReference target = root.child("eventList").push();
         // Retrieve key from new reference.
         String eventId = target.getKey();
         // Generate timestamp for event.
-        long date = Helper.createTimestamp();
+        // long date = Helper.createTimestamp();
+        long unixDate = date.getTime() / 1000;
         // Create new event object using combination of parameters and generated data.
-        Event newEvent = new Event(eventId, title, description, 0, date);
+        Event newEvent = new Event(eventId, title, description, 0, unixDate);
         // Set target DB ref to new object.
         target.setValue(newEvent);
 
