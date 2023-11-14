4807997c2a99055f0255c1dc05cbe02ceddc71d9
DavidIskandaryan <david.iskandaryan@yahoo.com>
2023-11-14 10:13:11 -0500

    Adding events page

---
+++ app/src/main/java/com/example/demoapplication/Student_complaints.java
@@ -0,0 +1,14 @@
+package com.example.demoapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Student_complaints extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_complaints);
+    }
+}
