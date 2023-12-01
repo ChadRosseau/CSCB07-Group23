0ac02cf8131884926d7880f3b37c6a42db100321
amanda_yin <yixin.yin@mail.utoronto.ca>
2023-12-01 01:32:47 -0500

    create event fragments xml and CreateEventFragmentView implemented.

--- app/src/main/java/com/example/demoapplication/fragments/events/CreateEventFragmentView.java
+++ app/src/main/java/com/example/demoapplication/fragments/events/CreateEventFragmentView.java
@@ -1,5 +1,7 @@
 package com.example.demoapplication.fragments.events;
 
+import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,20 +9,24 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
-import java.util.Calendar;
-
+import android.widget.Toast;
 
 import com.example.demoapplication.R;
 import com.example.demoapplication.fragments.BaseFragment;
 import com.example.demoapplication.presenters.subpresenters.admin.AdminAnnouncementsPresenter;
 import com.example.demoapplication.presenters.subpresenters.admin.AdminEventsPresenter;
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class CreateEventFragmentView extends BaseFragment {
     private AdminEventsPresenter presenter;
     private AdminAnnouncementsPresenter announcementsPresenter;
-    private EditText titleEditText, descriptionEditText, capacityEditText, locationEditText;
-    private DatePicker datePicker;
+    private EditText titleEditText, descriptionEditText, capacityEditText, locationEditText, dateEditText;
     private Button submitEventButton;
+    private Button selectDateTimeButton;
+    private Date timestamp;
 
     public CreateEventFragmentView() {}
 
@@ -37,51 +43,81 @@ public class CreateEventFragmentView extends BaseFragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.create_event, container, false);
 
-        // Initialize UI components
+        // Initialize all components
         titleEditText = view.findViewById(R.id.editTextText3);
         descriptionEditText = view.findViewById(R.id.editTextText4);
         capacityEditText = view.findViewById(R.id.editTextText5);
         locationEditText = view.findViewById(R.id.editTextText6);
-        datePicker = view.findViewById(R.id.datePicker);
+        selectDateTimeButton = view.findViewById(R.id.selectDateTimeButton);
         submitEventButton = view.findViewById(R.id.submitEventButton);
+        dateEditText = view.findViewById(R.id.dateEditText);
 
-        // Handle submit event button click
-        submitEventButton.setOnClickListener(new View.OnClickListener() {
+        // Set click listener for date and time button
+        selectDateTimeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Call ShowDatePickerDialog function for date and time picking.
+                showDatePickerDialog();
+            }
+        });
+
+        // Set click listener for submit button
+        submitEventButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
                 submitEvent();
+                activity.replaceFragment(new AdminEventsFragmentView());
             }
         });
 
         return view;
     }
 
-    // Get selected date from DatePicker
-    int day = datePicker.getDayOfMonth();
-    int month = datePicker.getMonth();
-    int year = datePicker.getYear();
-
-    // Convert date to timestamp
-    long timestamp = convertDateToTimestamp(day, month, year);
-
-// ...
-
-    // Helper method to convert date to timestamp
-    private long convertDateToTimestamp(int day, int month, int year) {
-        Calendar calendar = Calendar.getInstance();
-
-        calendar.set(Calendar.YEAR, year);
-        calendar.set(Calendar.MONTH, month);
-        calendar.set(Calendar.DAY_OF_MONTH, day);
-
-        // Set specific time components to default of the midnight
-        calendar.set(Calendar.HOUR_OF_DAY, 0);
-        calendar.set(Calendar.MINUTE, 0);
-        calendar.set(Calendar.SECOND, 0);
-        calendar.set(Calendar.MILLISECOND, 0);
-
-        return calendar.getTimeInMillis();
+    private void showDatePickerDialog() {
+        // Create a Date Picker Dialog
+        DatePickerDialog datePickerDialog = new DatePickerDialog(activity,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int date) {
+                        // Handle the selected date
+                        handleSelectedDate(year, month, date);
+                    }
+                },
+                // Set initial date to current date
+                Calendar.getInstance().get(Calendar.YEAR),
+                Calendar.getInstance().get(Calendar.MONTH),
+                Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+        );
+        // Show the dialog
+        datePickerDialog.show();
     }
+
+    private void handleSelectedDate(int year, int month, int dayOfMonth) {
+        // Convert date to a formatted string or handle as needed
+        String formattedDate = formatDate(year, month, dayOfMonth);
+
+        // Update the TextInputEditText
+        //TextInputEditText dateEditText = view.findViewById(R.id.dateEditText);
+        dateEditText.setText(formattedDate);
+
+        // Calculate timestamp with a specific starting time (e.g., 12:00 PM)
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month, dayOfMonth, 12, 0, 0);
+        timestamp = new Date(calendar.getTimeInMillis());
+    }
+
+    // ...
+
+    private String formatDate(int year, int month, int dayOfMonth) {
+        //  months are 0-indexed in Calendar
+        month += 1;
+
+        // Format the date using standard "MM/dd/yyyy" representation
+        String formattedMonth = (month < 10) ? "0" + month : String.valueOf(month);
+        String formattedDay = (dayOfMonth < 10) ? "0" + dayOfMonth : String.valueOf(dayOfMonth);
+
+        return formattedMonth + "/" + formattedDay + "/" + year;
+    }
+
 
 
     // Handle event submission
@@ -92,24 +128,7 @@ public class CreateEventFragmentView extends BaseFragment {
         int capacity = Integer.parseInt(capacityEditText.getText().toString().trim());
         String location = locationEditText.getText().toString().trim();
 
-        // Get selected date from DatePicker
-        int day = datePicker.getDayOfMonth();
-        int month = datePicker.getMonth() + 1; // Months are 0-indexed
-        int year = datePicker.getYear();
-
-        // Convert date to timestamp or handle as needed
-        // Example: long timestamp = convertDateToTimestamp(day, month, year);
-
-        /*
-         private void submitAnnouncement() {
-        String title = titleText.getText().toString();
-        String type = typeSpinner.getSelectedItem().toString();
-        String content = contentText.getText().toString();
-        presenter.createAnnouncement(title, type, content);
-    }
-         */
-        // Create an Event object and pass it to the presenter
-        //Event(String eventId, String title, String description, int attendeeCount, int maxAttendees, long date)
-        presenter.createEvent(title, description, 0, capacity, timestamp);
+        // *4th parameter is a long type.
+        presenter.createEvent(title, description, capacity, timestamp);//createEvent(String title, String description, int maxAttendees, Date date)
     }
 }
