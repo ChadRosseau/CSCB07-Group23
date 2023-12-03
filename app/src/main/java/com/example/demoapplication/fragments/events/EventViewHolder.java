ea1baa2d3956dcf30989d41b87e66e4294869500
Jean Luc Imanishimwe <imanjean30@gmail.com>
2023-12-02 20:06:24 -0500

    added the rsvp capability however w/o limit my attempts to add undo rsvp failed

--- app/src/main/java/com/example/demoapplication/fragments/events/EventViewHolder.java
+++ app/src/main/java/com/example/demoapplication/fragments/events/EventViewHolder.java
@@ -13,15 +13,15 @@ import com.example.demoapplication.R;
 import com.example.demoapplication.baseClasses.UserType;
 
 public class EventViewHolder extends RecyclerView.ViewHolder {
-    String eventId;
+    private String eventId;
     TextView name;
     TextView date;
     TextView location;
     TextView attendees;
     TextView description;
 
-    private Button feedbackButton;
-    private Button rsvpButton;
+    public Button feedbackButton;
+    public Button rsvpButton;
 
 
     public EventViewHolder(MainActivityView activity, View eventView) {
@@ -33,22 +33,6 @@ public class EventViewHolder extends RecyclerView.ViewHolder {
         description = eventView.findViewById(R.id.eventDescription);
         feedbackButton = eventView.findViewById(R.id.feedbackButton);
         rsvpButton = eventView.findViewById(R.id.rsvpButton);
-
-        boolean isAdmin = AuthModel.getInstance().getCurrentUserData().getUserType() == UserType.Admin;
-
-        if (isAdmin) {
-            feedbackButton.getLayoutParams().width = ViewGroup.LayoutParams.MATCH_PARENT;
-            feedbackButton.setText("VIEW FEEDBACK");
-            rsvpButton.setVisibility(View.GONE);
-        }
-
-        feedbackButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // TODO: situational for admin/student
-                activity.replaceFragment(isAdmin ? AdminEventsFeedbackView.newInstance(eventId) : new StudentEventsFeedbackView(), R.anim.transition_up);
-            }
-        });
     }
 
     public String getEventId() {
